8f357032a6c72da1a87eb11441ae6255eba28c84
notTGY <microcraft47@gmail.com>
2021-04-17 08:43:05 +0300

    start of page component

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function Page(props) {
+  const cellDPI = props.cellSize?props.cellSize*2:20
+
+
+  const [cid, setCid] = useState('')
+  const [c, setC] = useState({})
+
+  useEffect(_ => {
+    const val = 'canvas' + Math.floor(255*Math.random()).toString(16)
+    setCid(val)
+  }, [])
+
+  useEffect(_ => {
+    if (!cid) return
+    setC(document.getElementById(cid))
+  }, [cid])
+
+  useEffect(_ => {
+    if (!('id' in c)) return
+
+    c.style.width = (28*cellDPI / 2) + 'px'
+    c.style.height = (41*cellDPI / 2) + 'px'
+    c.width = 28*cellDPI
+    c.height = 41*cellDPI
+
+    const ctx = c.getContext('2d')
+    ctx.fillStyle = '#edbfb7'
+    ctx.fillRect(0,0,c.width,c.height)
+  }, [c])
+
+
+
   return (
     <div className='text-ink'>
-      Page
+      <canvas id={cid}/>
     </div>
   )
 }
